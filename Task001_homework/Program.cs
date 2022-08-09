85898fa10a5e8d19ffaa0b6079a01b601ea12daf
Zunickx <Zunickx@gmail.com>
2022-08-09 21:47:31 +0300

    Task001 (Задача на определение большего и меньшего из целых чисел

---
+++ Task001_homework/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA = -9;
+int numberB = -3;
+
+if(numberA > numberB)
+{
+    Console.WriteLine("Это число большее");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Это число меньшее", numberB);
+    Console.WriteLine(numberB);
+}
+else 
+{
+    Console.WriteLine("Это число большее", numberB);
+    Console.WriteLine(numberB);
+
+    Console.WriteLine("Это число меньшее", numberA);
+    Console.WriteLine(numberA);
+}
