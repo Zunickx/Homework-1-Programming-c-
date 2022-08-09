4864e48aaf89926b5abe37880a6621ecd77c058b
Zunickx <Zunickx@gmail.com>
2022-08-09 23:46:56 +0300

    Task003 (Задача на проверку четности числа)

---
+++ Task003_homework/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string numberA = Console.ReadLine();
+//int numberA = 10;
+
+if(int.Parse(numberA) % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    Console.WriteLine("Это нечетное число");
+}
