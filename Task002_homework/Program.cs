40f28b7608c63188e1072211d4a33b62155c3365
Zunickx <Zunickx@gmail.com>
2022-08-09 23:23:23 +0300

    Task002 (Максимальное число из трёх чисел)

---
+++ Task002_homework/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число a");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите число b");
+string numberB = Console.ReadLine();
+Console.WriteLine("Введите число c");
+string numberC = Console.ReadLine();
+
+if ((int.Parse(numberA) > int.Parse(numberB)) & (int.Parse(numberA) > int.Parse(numberC)))
+    {
+        Console.WriteLine("Max число");
+        Console.WriteLine(numberA);
+    }
+    else if ((int.Parse(numberB) > int.Parse(numberA)) & (int.Parse(numberB) > int.Parse(numberC)))
+    {
+        Console.WriteLine("Max число");
+        Console.WriteLine(numberB);
+    }
+    else
+    {
+        Console.WriteLine("Max число");
+        Console.WriteLine(numberC);
+    }
