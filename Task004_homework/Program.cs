dc687c225b0119a8d08b38823a609fe93e8970a5
Zunickx <Zunickx@gmail.com>
2022-08-10 22:12:16 +0300

    Task004 (Вывод четных чисел от 1 до N

---
+++ Task004_homework/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+string evenNumbers = Console.ReadLine();
+
+int i = 1;
+while(i <= int.Parse(evenNumbers))
+{
+    if(i % 2 == 0) {
+        Console.WriteLine(i);
+    }
+    i++;
+}
